f2169fc72cc872678332d480bc205d797d87a58a
Michael <p.michael.b@t-online.de>
2017-11-09 15:17:38 +0100

    Auto stash before merge of "master" and "origin/master"

---
+++ abc/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='abc',
+      version='0.1',
+      description='Implementation of different samplers for Applied Bayesian Computation (ABC)',
+      url='https://github.com/compercept/acm2017_abc/abc',
+      author='Michael Burkhardt, Fabian Kessler, Dominik Straub',
+      author_email='mbphoenix@t-online.de',
+      license='MIT',
+      packages=['abc'],
+      zip_safe=False)
