bb643b0b01e72aa85737162b420508b573290e34
Michael <p.michael.b@t-online.de>
2017-11-16 12:31:37 +0100

    Revert "add function for flatten list of outputs"
    This reverts commit d5b3cffcd05800ff4c5748996af9604303824254.

--- pyabc/sampler.py
+++ pyabc/sampler.py
@@ -230,9 +230,6 @@ class RejectionSampler(BaseSampler):
 
         return ret
 
-    def _flatten_output(self, x):
-        return np.hstack(np.atleast_1d(e).flatten() for e in x)
-
     def _run_rejection_sampling(self, distance):
         """the abc rejection sampling algorithm"""
 
@@ -262,7 +259,7 @@ class RejectionSampler(BaseSampler):
                     d = self.discrepancy(*[s(X) for s in self.summaries], *[s(Y) for s in self.summaries])
 
                 if d < self.threshold:
-                    thetas[i, :] = self._flatten_output(thetas_prop)
+                    thetas[i, :] = np.hstack(np.atleast_1d(p).flatten() for p in thetas_prop)
                     break
 
         self._runtime = time.clock() - start
