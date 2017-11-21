32461c80ff952a64c36e7a99c3e4e16c789cde95
Dominik Straub <dstraub93@gmail.com>
2017-11-21 13:23:08 +0100

    Prior class now converts string to scipy object

--- pyabc/__init__.py
+++ pyabc/__init__.py
@@ -1,5 +1,3 @@
 from .rejection_sampler import RejectionSampler
 from .smc_sampler import SMCSampler
-from .prior import UniformPrior
-from .prior import GaussianPrior
 from .prior import Prior
--- pyabc/utils.py
+++ pyabc/utils.py
@@ -1,4 +1,20 @@
 import numpy as np
+import scipy.stats as ss
+
+SCIPY_ALIASES = {
+    'normal': 'norm',
+    'gaussian': 'norm',
+    'exponential': 'expon',
+    'unif': 'uniform',
+    'bin': 'binom',
+    'binomial': 'binom'
+}
+
+def scipy_from_str(name):
+    """Return the scipy.stats distribution corresponding to `name`."""
+    name = name.lower()
+    name = SCIPY_ALIASES.get(name, name)
+    return getattr(ss, name)
 
 def flatten_function(list_of_f, args=None):
     """return function output as 1d array"""
