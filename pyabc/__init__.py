65c1905a51a45dfa29b51919250e453f168cb2f9
Dominik Straub <dstraub93@gmail.com>
2017-11-24 13:55:14 +0100

    Add BOLFI class skeleton

--- pyabc/__init__.py
+++ pyabc/__init__.py
@@ -1,3 +1,7 @@
+# prevent warning from GPyOpt, which also tries to set the mpl backend
+import matplotlib as mpl
+mpl.use('Agg')
 from .rejection_sampler import RejectionSampler
 from .smc_sampler import SMCSampler
 from .prior import Prior
+from .bolfi import BOLFI
