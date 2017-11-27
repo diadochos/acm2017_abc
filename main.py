8f7021ced186a51a846db10721994a1516997d16
Michael <p.michael.b@t-online.de>
2017-11-27 16:27:38 +0100

    np updated, main contains BOLFI test

--- main.py
+++ main.py
@@ -1,19 +1,36 @@
 import numpy as np
-import sys, os
+import pylab as plt
+from collections import Counter
+import os
+import sys
 sys.path.append(os.path.abspath(os.path.join('..')))
-from pyabc import BOLFI, Prior
-import emcee
-import scipy.stats as ss
-import matplotlib.pyplot as plt
-from pyabc.plots import plot_marginals
+from pyabc.examples import tuberculosis
+import pyabc.prior
+from pyabc.plots import plot_marginals, plot_particles
 
-mu0 = 2.5
-y0 = np.random.normal(mu0, 1, 2)
 
-prior = Prior('uniform', 0, 5)
+alpha = 0.2
+delta = 0
+tau = 0.198
+m = 20
 
-def simulator(mu):
-    return np.random.normal(mu, 1, 2)
+prior_alpha = pyabc.Prior("uniform", 0.1, 1, name="alpha")
+prior_tau = pyabc.Prior("uniform", 0.01, 1, name="tau")
+domain = [(0.1, 1), (0.01, 1)]
 
-bolfi = BOLFI(priors=[prior], simulator=simulator, observation=y0, summaries=[np.mean], domain=[(0, 5)])
-thetas = bolfi.sample(nr_samples=2000, n_chains=6)
+simulator = tuberculosis.simulator
+
+for i in range(100):
+    params = [prior_alpha.sample(), prior_tau.sample()]
+    y0 = simulator(*params)
+
+bolfi = pyabc.BOLFI(
+    priors=[prior_alpha, prior_tau],
+    simulator=simulator,
+    summaries=tuberculosis.summaries,
+    observation=y0,
+    seed = 1337,
+    domain=domain
+)
+
+bolfi.sample(nr_samples=10000, n_chains=10)
